0c816b53b51aad95ba21f1228be2ec8fa16ab56e
Ukendiran <ukendiran@gmail.com>
2025-11-20 16:32:21 +0530

    auth:fix

--- server/auth.ts
+++ server/auth.ts
@@ -24,6 +24,7 @@ export function getSession() {
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: sessionTtl,
     },
   });
