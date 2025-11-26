49b23a125a1d38162ca3dd2b219daf68c4348c96
Karthi417 <118118679+Karthi417@users.noreply.github.com>
2025-11-26 12:23:42 +0530

    V 1.0.2

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
           </div>
 
           <p className="mt-4 text-sm text-gray-600">
-            Did you forget to add the page to the router?
+            Oops, the page you're looking for doesn't exist or you don't have authentication to access it?
           </p>
         </CardContent>
       </Card>
